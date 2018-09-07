1920a8dc7894da72f40abbf6365cbebde35af012
tkmam0t <tanwinder.sidhu@kohls.com>
2018-09-07 11:58:32 -0500

    add babelrc

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/app.scss';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-               Hello 
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className='app'>
+        		Hello 
+				<div>
+					kidaa
+					<ul>
+
+					</ul>
+				</div>
+		    </div>
+		);
+	}
 }
 
 export default App;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,35 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [ 'babel-loader', 'eslint-loader' ]
+			},
+			{
+				test: /\.html$/,
+				use: [ 'html-loader' ]
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract(
+					{
+						fallback: 'style-loader',
+						use: [ 'css-loader', 'sass-loader' ]
+					})
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin(
+			{ filename: 'style.css' }
+		),
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html'
+		})
+	]
 };
