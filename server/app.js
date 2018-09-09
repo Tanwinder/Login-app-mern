97c897f0e2693599a48cd2d80afe0c179329569d
tkmam0t <tanwinder.sidhu@kohls.com>
2018-09-09 14:53:52 -0500

    add error handling for api

--- server/app.js
+++ server/app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
