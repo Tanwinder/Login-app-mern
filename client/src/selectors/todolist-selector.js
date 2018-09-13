03bab6c1ec657fb32ad79ecd9f7ddf44f0e556e0
tkmam0t <tanwinder.sidhu@kohls.com>
2018-09-13 13:24:18 -0500

    add purecomponents and reselect for selectors

--- client/src/selectors/todolist-selector.js
+++ client/src/selectors/todolist-selector.js
@@ -1,1 +1,19 @@
-export const getTodoItems = state => state.todolist.items;
+import {createSelector} from 'reselect';
+
+const getTodoItems = state => state.todolist.items;
+const getTodoFilter = state => state.todolist.filter;
+
+export const getFilteredTodoItems = createSelector(
+	[ getTodoItems, getTodoFilter ], (todo, filter) => {
+		switch(filter) {
+		case 'all':
+			return todo;
+		case 'completed':
+			return todo.filter( ac => ac.isChecked);
+		case 'pending':
+			return todo.filter( ac => !ac.isChecked);
+		default:
+			return todo;
+		}
+	}
+);
