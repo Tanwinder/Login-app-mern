1caf692a318ac74afbed8a1ec02781b501445a65
tkmam0t <tanwinder.sidhu@kohls.com>
2018-11-18 22:58:36 -0600

    add prouct post method

--- server/api/routes/products.js
+++ server/api/routes/products.js
@@ -21,23 +21,33 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const product = new Product({
-        name: req.body.name,
-        price: req.body.price
-    });
-
-    product
-    .save()
-    .then( result => {
-        console.log(result);
-        res.status(200).json({
-            message: 'save product successfully',
-            newProduct: result
-        })
-    })
-    .catch( err => {
-        console.log(err);
-        res.status(500).json({error: err});
+    Product.findOne({name: req.body.name})
+    .then( item => {
+        if( item !== null ) {
+            res.status(201).json({
+                message: 'Item is already present in the database',
+                newProduct: null
+            })
+        } else {
+            const product = new Product({
+                name: req.body.name,
+                price: req.body.price
+            });
+        
+            product
+            .save()
+            .then( result => {
+                console.log(result);
+                res.status(200).json({
+                    message: 'save product successfully',
+                    newProduct: result
+                })
+            })
+            .catch( err => {
+                console.log(err);
+                res.status(500).json({error: err});
+            })
+        }
     })
 });
 
@@ -69,9 +79,26 @@ router.delete('/:productId', (req, res, next) => {
 
 router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message: 'product detail PATCH request done',
-        id: id
+    const updateOps = {};
+    for ( let ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product
+    .update({_id: id}, {$set: updateOps})
+    .exec()
+    .then( result => {
+        res.status(200).json({
+            message: 'product detail PATCH request done',
+            request: {
+                type: 'GET',
+                url: 'http://localhost:4000/products/' + result._id
+            }
+        })
+    })
+    .catch( err => {
+        res.status(500).json({
+            error: err
+        })
     })
 });
 
